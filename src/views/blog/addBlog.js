aec19512658f6827860752be829d689051ed3fca
Aravin7 <aravinth.ati@gmail.com>
2022-02-13 22:01:37 +0530

    create blog component

--- src/views/blog/addBlog.js
+++ src/views/blog/addBlog.js
@@ -1,3 +1,13 @@
+import {
+  CCol,
+  CContainer,
+  CForm,
+  CFormGroup,
+  CFormText,
+  CInput,
+  CLabel,
+  CRow,
+} from "@coreui/react";
 import React from "react";
 //import { useHistory } from 'react-dom';
 
@@ -6,6 +16,40 @@ const AddBlog = () => {
   return (
     <div>
       <p>New Blog</p>
+      <CContainer fluid>
+        <CRow>
+          <CCol sm="12">
+            <CForm action="" method="post">
+              <CFormGroup>
+                <CLabel htmlFor="nf-email">Email</CLabel>
+                <CInput
+                  type="email"
+                  id="nf-email"
+                  name="nf-email"
+                  placeholder="Enter Email.."
+                  autoComplete="email"
+                />
+                <CFormText className="help-block">
+                  Please enter your email
+                </CFormText>
+              </CFormGroup>
+              <CFormGroup>
+                <CLabel htmlFor="nf-password">Password</CLabel>
+                <CInput
+                  type="password"
+                  id="nf-password"
+                  name="nf-password"
+                  placeholder="Enter Password.."
+                  autoComplete="current-password"
+                />
+                <CFormText className="help-block">
+                  Please enter your password
+                </CFormText>
+              </CFormGroup>
+            </CForm>
+          </CCol>
+        </CRow>
+      </CContainer>
     </div>
   );
 };
